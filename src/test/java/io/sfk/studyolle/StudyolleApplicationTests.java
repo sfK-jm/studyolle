ff8cfef4bc5aa441eb93ebfd1c4600d9c85a0772
sfK <sfk.sjm@gmail.com>
2025-02-01 20:49:51 +0900

    project created

---
+++ src/test/java/io/sfk/studyolle/StudyolleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sfk.studyolle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyolleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
