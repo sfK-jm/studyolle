6c8365c1a78f174438e374b5015bc2d30731628b
sfK <sfk.sjm@gmail.com>
2025-02-24 23:18:02 +0900

    프로필 이미지 변경

--- src/main/java/io/sfk/studyolle/account/AccountService.java
+++ src/main/java/io/sfk/studyolle/account/AccountService.java
@@ -102,7 +102,7 @@ public class AccountService implements UserDetailsService {
         account.setOccupation(profile.getOccupation());
         account.setLocation(profile.getLocation());
         account.setBio(profile.getBio());
-        // TODO: 프로필 이미지
+        account.setProfileImage(profile.getProfileImage());
         accountRepository.save(account);
     }
 }
