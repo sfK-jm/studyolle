5494649a87a38cdf367ace296e1c71947c4a6209
sfK <sfk.sjm@gmail.com>
2025-03-16 22:04:28 +0900

    스터디 설정 - 소개 수정

--- src/main/java/io/sfk/studyolle/domain/Account.java
+++ src/main/java/io/sfk/studyolle/domain/Account.java
@@ -69,4 +69,8 @@ public class Account {
 
         //return this.emailCheckTokenGeneratedAt.isBefore(LocalDateTime.now().minusHours(1));
     }
+
+    public boolean isManagerOf(Study study) {
+        return study.getManagers().contains(this);
+    }
 }
