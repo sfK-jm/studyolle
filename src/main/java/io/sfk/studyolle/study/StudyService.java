82b9df727f45e00a399f3d29b6e6ac94b50e7cb8
sfK <sfk.sjm@gmail.com>
2025-03-19 23:29:39 +0900

    스터디 설정 - 상태 변경

--- src/main/java/io/sfk/studyolle/study/StudyService.java
+++ src/main/java/io/sfk/studyolle/study/StudyService.java
@@ -71,14 +71,21 @@ public class StudyService {
     }
 
     public Study getStudyToUpdateTag(Account account, String path) {
-        Study study = repository.findAccountWithTagsByPath(path);
+        Study study = repository.findStudyWithTagsByPath(path);
         checkIfExistingStudy(path, study);
         checkIfManger(account, study);
         return study;
     }
 
     public Study getStudyToUpdateZone(Account account, String path) {
-        Study study = repository.findAccountWithZonesByPath(path);
+        Study study = repository.findStudyWithZonesByPath(path);
+        checkIfExistingStudy(path, study);
+        checkIfManger(account, study);
+        return study;
+    }
+
+    public Study getStudyToUpdateStatus(Account account, String path) {
+        Study study = repository.findStudyWithManagersByPath(path);
         checkIfExistingStudy(path, study);
         checkIfManger(account, study);
         return study;
@@ -94,5 +101,22 @@ public class StudyService {
         if (study == null) {
             throw new IllegalArgumentException(path + "에 해당하는 스터디가 없습니다.");
         }
+    }
+
+    public void publish(Study study) {
+        study.publish();
+    }
+
+    public void close(Study study) {
+        study.close();
+    }
+
+    public void startRecruit(Study study) {
+        study.startRecruit();
+
+    }
+
+    public void stopRecruit(Study study) {
+        study.stopRecruit();
     }
 }
