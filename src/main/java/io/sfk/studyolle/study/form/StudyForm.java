5d631db38d1c878cb579d8111ae6bb0a3747e649
sfK <sfk.sjm@gmail.com>
2025-03-20 11:52:10 +0900

    스터디 설정 - 경로 및 이름 수정

--- src/main/java/io/sfk/studyolle/study/form/StudyForm.java
+++ src/main/java/io/sfk/studyolle/study/form/StudyForm.java
@@ -1,14 +1,18 @@
 package io.sfk.studyolle.study.form;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
 @Data
 public class StudyForm {
 
+    public static final String VALID_PATH_PATTERN = "^[ㄱ-ㅎ가-힣a-z0-9_-]{2,20}$";
+
     @NotBlank
     @Length(min = 2, max = 20)
+    @Pattern(regexp = VALID_PATH_PATTERN)
     private String path;
 
     @NotBlank
