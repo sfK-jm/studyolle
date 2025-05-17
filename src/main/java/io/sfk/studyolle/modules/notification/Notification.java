d82cc2a99ae3f8d7a113fa6a51ebd12c1f4e2f61
sfK <sfk.sjm@gmail.com>
2025-05-17 12:45:39 +0900

    알림 도메인

---
+++ src/main/java/io/sfk/studyolle/modules/notification/Notification.java
@@ -0,0 +1,34 @@
+package io.sfk.studyolle.modules.notification;
+
+import io.sfk.studyolle.modules.account.Account;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@EqualsAndHashCode(of = "id")
+public class Notification {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    private String link;
+
+    private String message;
+
+    private boolean checked;
+
+    @ManyToOne
+    private Account account;
+
+    private LocalDateTime createdLocalDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationType notificationType;
+}
---
+++ src/main/java/io/sfk/studyolle/modules/notification/NotificationType.java
@@ -0,0 +1,6 @@
+package io.sfk.studyolle.modules.notification;
+
+public enum NotificationType {
+    STUDY_CREATED, STUDY_UPDATED, EVENT_ENROLLMENT;
+
+}
