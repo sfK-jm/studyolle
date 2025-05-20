d71f486f8e6360c627ea4ed5e2fc5504112ad7fc
sfK <sfk.sjm@gmail.com>
2025-05-20 20:53:49 +0900

    알림 아이콘 생성

--- src/main/java/io/sfk/studyolle/modules/notification/NotificationRepository.java
+++ src/main/java/io/sfk/studyolle/modules/notification/NotificationRepository.java
@@ -1,6 +1,9 @@
 package io.sfk.studyolle.modules.notification;
 
+import io.sfk.studyolle.modules.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    long countByAccountAndChecked(Account account, boolean checked);
 }
