01d3b2683358dc15f19122c640230609950469f3
sfK <sfk.sjm@gmail.com>
2025-05-21 21:57:04 +0900

    웹 알림 목록 조회 및 삭제

--- src/main/java/io/sfk/studyolle/modules/notification/NotificationRepository.java
+++ src/main/java/io/sfk/studyolle/modules/notification/NotificationRepository.java
@@ -2,8 +2,18 @@ package io.sfk.studyolle.modules.notification;
 
 import io.sfk.studyolle.modules.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@Transactional(readOnly = true)
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
     long countByAccountAndChecked(Account account, boolean checked);
+
+    @Transactional
+    List<Notification> findByAccountAndCheckedOrderByCreatedDateTimeDesc(Account account, boolean checked);
+
+    @Transactional
+    void deleteByAccountAndChecked(Account account, boolean checked);
 }
