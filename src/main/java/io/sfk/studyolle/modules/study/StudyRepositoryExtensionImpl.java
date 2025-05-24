ed09c7f928897bed465c77b000645bb20043a55e
sfK <sfk.sjm@gmail.com>
2025-05-24 14:23:19 +0900

    querydsl n + 1 해결 및 검색 결과에 페이징 적용

--- src/main/java/io/sfk/studyolle/modules/study/StudyRepositoryExtensionImpl.java
+++ src/main/java/io/sfk/studyolle/modules/study/StudyRepositoryExtensionImpl.java
@@ -1,9 +1,14 @@
 package io.sfk.studyolle.modules.study;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.JPQLQuery;
+import io.sfk.studyolle.modules.account.QAccount;
+import io.sfk.studyolle.modules.tag.QTag;
+import io.sfk.studyolle.modules.zone.QZone;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
-
-import java.util.List;
 
 public class StudyRepositoryExtensionImpl extends QuerydslRepositorySupport implements StudyRepositoryExtension {
 
@@ -12,12 +17,20 @@ public class StudyRepositoryExtensionImpl extends QuerydslRepositorySupport impl
     }
 
     @Override
-    public List<Study> findByKeyword(String keyword) {
+    public Page<Study> findByKeyword(String keyword, Pageable pageable) {
         QStudy study = QStudy.study;
-        JPQLQuery<Study> query = from(study).where(study.published.isTrue()
-                .and(study.title.containsIgnoreCase(keyword))
-                .or(study.tags.any().title.containsIgnoreCase(keyword))
-                .or(study.zones.any().localNameOfCity.containsIgnoreCase(keyword)));
-        return query.fetch();
+        JPQLQuery<Study> query = from(study)
+                .where(study.published.isTrue()
+                        .and(study.title.containsIgnoreCase(keyword))
+                        .or(study.tags.any().title.containsIgnoreCase(keyword))
+                        .or(study.zones.any().localNameOfCity.containsIgnoreCase(keyword)))
+                .leftJoin(study.tags, QTag.tag).fetchJoin()
+                .leftJoin(study.zones, QZone.zone).fetchJoin()
+                .leftJoin(study.members, QAccount.account).fetchJoin()
+                .distinct();
+
+        JPQLQuery<Study> pageableQuery = getQuerydsl().applyPagination(pageable, query);
+        QueryResults<Study> fetchResults = pageableQuery.fetchResults();
+        return new PageImpl<>(fetchResults.getResults(), pageable, fetchResults.getTotal());
     }
 }
